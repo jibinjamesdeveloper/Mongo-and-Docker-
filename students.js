c8997027633bf816a7899c0231f2b447235ef08e
jibinjamesdeveloper <jibinjames158@gmail.com>
2021-06-13 22:17:55 +0530

    Mongo db commands , docker file for mongo and mongo express

---
+++ students.js
@@ -0,0 +1,53 @@
+student = [{
+    "firstName": "Jibin",
+    "lastName": "James",
+    "email": "jibinjames158@gmail.com",
+    "gender": "M",
+    "country": "India",
+    "isStudentActive": false,
+    "favoriteSubjects": [
+        "maths",
+        "english",
+        "it"
+    ],
+    "totalSpentInBooks": 0.00
+},{
+    "firstName": "Alen",
+    "lastName": "paul",
+    "email": "alen@gmail.com",
+    "gender": "M",
+    "country": "India",
+    "isStudentActive": false,
+    "favoriteSubjects": [
+        "maths",
+        "english",
+        "Malayalam"
+    ],
+    "totalSpentInBooks": 10.00
+},
+{
+    "firstName": "Sam",
+    "lastName": "Samuel",
+    "email": "sam@gmail.com",
+    "gender": "M",
+    "country": "India",
+    "isStudentActive": false,
+    "favoriteSubjects": [
+        "maths",
+        "english",
+        "it"
+    ],
+    "totalSpentInBooks": 100.00
+},{
+    "firstName": "Amal",
+    "lastName": "Jose",
+    "email": "amal8@gmail.com",
+    "gender": "M",
+    "country": "India",
+    "isStudentActive": false,
+    "favoriteSubjects": [
+        "maths"
+    ],
+    "totalSpentInBooks": 0.00
+}
+]
